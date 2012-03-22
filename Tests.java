81bc02ebf96c15b49d6238320522a30b0777d799
ewerlopes <ewerton.lopes@dce.ufpb.br>
2012-03-22 13:33:41 -0300

    Tests calls updated! Now it has two test methods.

--- Tests.java
+++ Tests.java
@@ -16,46 +16,93 @@ import br.edu.ufcg.threadcontrol.ThreadState;
 public class Tests {
 
 	@Test
-	public void addTest() throws InterruptedException {
+	public void testOfAdding() throws InterruptedException {
 		
-		ThreadControl tc = new ThreadControl(); //It creates a new instance of ThreadControl Class...
-		Server server = new Server();// It creates a new instance of server class. This object has the fundamental methods to the program.
-		BlockingQueue <List<String>> responseList = new LinkedBlockingQueue<List<String>>(); //it creates a blockingqueue element that will be needed to save the method results
+		ThreadControl tc = new ThreadControl(); //Creates a new instance of ThreadControl Class...
+		Server server = new Server();// Creates a new instance of server class. This object has the fundamental methods to the program.
+		/*Creates a BlockingQueue object which will serve to catch the data which have been stored on Server Object.*/
+		BlockingQueue <List<String>> responseList = new LinkedBlockingQueue<List<String>>(); 
 		
+		/*gets a object from server, saving it into responseList*/
 		server.getObjectList(responseList);
 		
+		/* Puts the responseList result into  retrieveData variable.*/
 		List<String> currentObjects = responseList.take();
 		
-		assertTrue(currentObjects.size() ==0);
-		tc.prepare(getSavingOperationHasTerminated() );
+		//BE CAREFUL HERE...This assertion isn't protected by TC.
+		assertTrue(currentObjects.size() ==0); 
+		/*Sets a state must be expected by test thread*/
+		tc.prepare(getStoringOperationHasTerminated() );
+		/*Adds a String into server*/
 		server.storeMethod("Test");
-		
-		tc.waitUntilStateIsReached(); //Faz a thread do teste esperar até que o estado esperado esteja pronto;
+		/*Forces the test thread to wait until the state, configured up by prepare method, be reached */
+		tc.waitUntilStateIsReached();
+		/*gets a object from server, saving it into responseList*/
 		server.getObjectList(responseList);
+		/*Sets a new state must be expected by test thread*/
 		tc.prepare(getRetrievingOperationHasTerminated());
+		/*Frees possible thread which were blocked by the first call of waitUntilStateIsReached*/
 		tc.proceed();
 		
 		currentObjects = responseList.take();
+		
+		/*Forces the test thread to wait until the state, configured up by prepare method, be reached */
 		tc.waitUntilStateIsReached();
-
+		/*Performs the JUNIT assertion, verifying if the retrieved data from server object is the same that was added on it*/
 		assertTrue(currentObjects.size() == 1);
-		assertTrue(currentObjects.get(0).equals("Test"));
+		/*Frees possible thread which were blocked by the last waitUntilStateIsReached allowing the test to continue*/
 		tc.proceed();
 		
 	}
 	
-	public SystemConfiguration getSavingOperationHasTerminated(){
+	@Test
+	public void testOfRetrieving() throws InterruptedException{
+		/*
+		Creates a BlockingQueue object which will serve to catch the data which have been stored on Server Object.
+		*/
+		BlockingQueue <List<String>> responseList = new LinkedBlockingQueue<List<String>>(); 
+		/*Creates a new server object*/
+		Server server = new Server();
+		/*Creates a new ThreadControl object which will serve to perform test primitives of TC approach*/
+		ThreadControl tc = new ThreadControl();
+		
+		/*Sets a state must be expected by test thread*/
+		tc.prepare(getStoringOperationHasTerminated());
+		/*Adds a String into server*/
+		server.storeMethod("This is a test!");
+		/*Forces the test thread to wait until the state, configured up by prepare method, be reached */
+		tc.waitUntilStateIsReached();
+		/*Frees possible thread which were blocked by waitUntilStateIsReached*/
+		tc.proceed();
+		/*Sets a new state must be expected by test thread*/
+		tc.prepare(getRetrievingOperationHasTerminated());
+		/*gets a object from server, saving it into responseList*/
+		server.getObjectList(responseList);
+		/*Forces the test thread to wait until the state, configured up by the last prepare method, be reached */
+		tc.waitUntilStateIsReached();
+		/*Frees possible thread which were blocked by the last waitUntilStateIsReached*/
+		tc.proceed();
+		/* Puts the responseList result into  retrieveData variable.*/
+		List<String> retrievedData= responseList.take();
+		/*Performs the JUNIT assertion, verifying if the retrieved data from server object is the same that was added on it*/
+		assertTrue(retrievedData.get(0).equals("This is a test!"));
+				
+	}
+	
+	
+	/*Sets a wanted state of System*/
+	public SystemConfiguration getStoringOperationHasTerminated(){
 		ThreadConfiguration config1 = new ThreadConfiguration(Server.threadOfStoring.class.getCanonicalName(), ThreadState.FINISHED, ThreadConfiguration.AT_LEAST_ONCE);
 		ListOfThreadConfigurations sysConfig = new ListOfThreadConfigurations();
 		sysConfig.addThreadConfiguration(config1);
 		return sysConfig;
 	}
 	
+	/*Sets a wanted state of System*/
 	public SystemConfiguration getRetrievingOperationHasTerminated(){
 		ThreadConfiguration config1 = new ThreadConfiguration(Server.threadOfRetrieving.class.getCanonicalName(), ThreadState.FINISHED, ThreadConfiguration.AT_LEAST_ONCE);
 		ListOfThreadConfigurations sysConfig = new ListOfThreadConfigurations();
 		sysConfig.addThreadConfiguration(config1);
 		return sysConfig;
 	}
-
 }
